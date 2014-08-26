95eba7d7d3b25381581dda19c8b6a7074587240a
Katy Huff <katyhuff@gmail.com>
2014-08-25 22:44:07 -0700

    blind attempt

--- src/streamblender_tests.cc
+++ src/streamblender_tests.cc
@@ -1,21 +1,21 @@
 #include <gtest/gtest.h>
 
-#include "separationmatrix_tests.h"
+#include "streamblender_tests.h"
 
-namespace separationmatrix {
+namespace streamblender {
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-void SeparationMatrixTest::SetUp() {
-  src_facility_ = new SeparationMatrix(tc_.get());
+void StreamBlenderTest::SetUp() {
+  src_facility_ = new StreamBlender(tc_.get());
   InitParameters();
-  SetUpSeparationMatrix();
+  SetUpStreamBlender();
 }
 
-void SeparationMatrixTest::TearDown() {
+void StreamBlenderTest::TearDown() {
   delete src_facility_;
 }
 
-void SeparationMatrixTest::InitParameters(){
+void StreamBlenderTest::InitParameters(){
   in_c1 = "in_c1";
   out_c1 = "out_c1";
   out_c2 = "out_c2";
@@ -30,7 +30,7 @@ void SeparationMatrixTest::InitParameters(){
 
 }
 
-void SeparationMatrixTest::SetUpSeparationMatrix(){
+void StreamBlenderTest::SetUpStreamBlender(){
   src_facility_->in_commod_(in_c1);
   src_facility_->out_commods_(outs);
   src_facility_->process_time_(process_time);
@@ -38,7 +38,7 @@ void SeparationMatrixTest::SetUpSeparationMatrix(){
   src_facility_->capacity_(capacity);
 }
 
-void SeparationMatrixTest::TestInitState(SeparationMatrix* fac){
+void StreamBlenderTest::TestInitState(StreamBlender* fac){
   EXPECT_EQ(process_time, fac->process_time_());
   EXPECT_EQ(max_inv_size, fac->max_inv_size_());
   EXPECT_EQ(capacity, fac->capacity_());
@@ -46,43 +46,43 @@ void SeparationMatrixTest::TestInitState(SeparationMatrix* fac){
   EXPECT_EQ(in_c1, fac->in_commod_());
 }
 
-void SeparationMatrixTest::TestRequest(SeparationMatrix* fac, double cap){
+void StreamBlenderTest::TestRequest(StreamBlender* fac, double cap){
   //cyclus::Material::Ptr req = fac->Request_();
   //EXPECT_EQ(cap, req->quantity());
 }
 
-void SeparationMatrixTest::TestAddMat(SeparationMatrix* fac, 
+void StreamBlenderTest::TestAddMat(StreamBlender* fac, 
     cyclus::Material::Ptr mat){
   double amt = mat->quantity();
-  double before = fac->sepbuff_quantity();
+  double before = fac->blendbuff.quantity();
   //fac->AddMat_(mat);
-  double after = fac->sepbuff_quantity();
+  double after = fac->blendbuff.quantity();
   EXPECT_EQ(amt, after - before);
 }
 
-void SeparationMatrixTest::TestBuffers(SeparationMatrix* fac, double inv, 
-    double proc, double rawbuff){
+void StreamBlenderTest::TestBuffers(StreamBlender* fac, double inv, 
+    double proc, double rawbuffs){
   double t = tc_.get()->time();
 
-  EXPECT_EQ(inv, fac->sepbuff_quantity());
-  EXPECT_EQ(rawbuff, fac->rawbuff.quantity());
+  EXPECT_EQ(inv, fac->blendbuff.quantity());
+  EXPECT_EQ(rawbuffs, fac->rawbuffs_quantity());
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(SeparationMatrixTest, clone) {
-  SeparationMatrix* cloned_fac =
-      dynamic_cast<SeparationMatrix*> (src_facility_->Clone());
+TEST_F(StreamBlenderTest, clone) {
+  StreamBlender* cloned_fac =
+      dynamic_cast<StreamBlender*> (src_facility_->Clone());
   TestInitState(cloned_fac);
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(SeparationMatrixTest, InitialState) {
+TEST_F(StreamBlenderTest, InitialState) {
   // Test things about the initial state of the facility here
   TestInitState(src_facility_);
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(SeparationMatrixTest, CurrentCapacity) {
+TEST_F(StreamBlenderTest, CurrentCapacity) {
   EXPECT_EQ(capacity, src_facility_->current_capacity());
   src_facility_->max_inv_size_(1e299);
   EXPECT_EQ(1e299, src_facility_->max_inv_size_());
@@ -91,18 +91,18 @@ TEST_F(SeparationMatrixTest, CurrentCapacity) {
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(SeparationMatrixTest, Print) {
+TEST_F(StreamBlenderTest, Print) {
   EXPECT_NO_THROW(std::string s = src_facility_->str());
-  // Test SeparationMatrix specific aspects of the print method here
+  // Test StreamBlender specific aspects of the print method here
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(SeparationMatrixTest, Request) { 
+TEST_F(StreamBlenderTest, Request) { 
   TestRequest(src_facility_, src_facility_->current_capacity());
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(SeparationMatrixTest, AddMats) { 
+TEST_F(StreamBlenderTest, AddMats) { 
   double cap = src_facility_->current_capacity();
   cyclus::Material::Ptr mat = cyclus::NewBlankMaterial(0.5*cap);
   TestAddMat(src_facility_, mat);
@@ -113,13 +113,13 @@ TEST_F(SeparationMatrixTest, AddMats) {
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(SeparationMatrixTest, Tick) {
+TEST_F(StreamBlenderTest, Tick) {
   ASSERT_NO_THROW(src_facility_->Tick());
-  // Test SeparationMatrix specific behaviors of the Tick function here
+  // Test StreamBlender specific behaviors of the Tick function here
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(SeparationMatrixTest, Tock) {
+TEST_F(StreamBlenderTest, Tock) {
 
   // initially, nothing in the buffers
   TestBuffers(src_facility_,0,0,0);
@@ -129,7 +129,7 @@ TEST_F(SeparationMatrixTest, Tock) {
   //cyclus::Material::Ptr mat = cyclus::Material::CreateUntracked(cap, rec);
   //TestAddMat(src_facility_, mat);
 
-  // affter add, the sepbuff has the material
+  // affter add, the blendbuff has the material
   TestBuffers(src_facility_,cap,0,0);
 
   EXPECT_NO_THROW(src_facility_->Tock());
@@ -158,7 +158,7 @@ TEST_F(SeparationMatrixTest, Tock) {
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-TEST_F(SeparationMatrixTest, NoProcessTime) {
+TEST_F(StreamBlenderTest, NoProcessTime) {
   // tests what happens when the process time is zero
   src_facility_->process_time_(0);
   EXPECT_EQ(0, src_facility_->process_time_());
@@ -168,21 +168,21 @@ TEST_F(SeparationMatrixTest, NoProcessTime) {
   //cyclus::Material::Ptr mat = cyclus::Material::CreateUntracked(cap, rec);
   //TestAddMat(src_facility_, mat);
 
-  // affter add, the sepbuff has the material
+  // affter add, the blendbuff has the material
   TestBuffers(src_facility_,cap,0,0);
 
   EXPECT_NO_THROW(src_facility_->Tock());
 
-  // affter tock, the rawbuff have the material
+  // affter tock, the rawbuffs have the material
   TestBuffers(src_facility_,0,0,cap);
 }
 
 
-} // namespace separationmatrix
+} // namespace streamblender
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-cyclus::Agent* SeparationMatrixConstructor(cyclus::Context* ctx) {
-  return new separationmatrix::SeparationMatrix(ctx);
+cyclus::Agent* StreamBlenderConstructor(cyclus::Context* ctx) {
+  return new streamblender::StreamBlender(ctx);
 }
 
 // required to get functionality in cyclus agent unit tests library
@@ -193,8 +193,8 @@ static int cyclus_agent_tests_connected = ConnectAgentTests();
 #endif // CYCLUS_AGENT_TESTS_CONNECTED
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-INSTANTIATE_TEST_CASE_P(SeparationMatrixFac, FacilityTests,
-                        ::testing::Values(&SeparationMatrixConstructor));
+INSTANTIATE_TEST_CASE_P(StreamBlenderFac, FacilityTests,
+                        ::testing::Values(&StreamBlenderConstructor));
 
-INSTANTIATE_TEST_CASE_P(SeparationMatrixFac, AgentTests,
-                        ::testing::Values(&SeparationMatrixConstructor));
+INSTANTIATE_TEST_CASE_P(StreamBlenderFac, AgentTests,
+                        ::testing::Values(&StreamBlenderConstructor));
--- src/streamblender_tests.h
+++ src/streamblender_tests.h
@@ -1,30 +1,30 @@
-#ifndef SEPARATIONMATRIX_TESTS_H_
-#define SEPARATIONMATRIX_TESTS_H_
+#ifndef STREAMBLENDER_TESTS_H_
+#define STREAMBLENDER_TESTS_H_
 
 #include <gtest/gtest.h>
 
-#include "separationmatrix.h"
+#include "streamblender.h"
 
 #include "context.h"
 #include "facility_tests.h"
 #include "agent_tests.h"
 
-namespace separationmatrix {
+namespace streamblender {
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-class SeparationMatrixTest : public ::testing::Test {
+class StreamBlenderTest : public ::testing::Test {
  protected:
   cyclus::TestContext tc_;
-  SeparationMatrix* src_facility_;
+  StreamBlender* src_facility_;
 
   virtual void SetUp();
   virtual void TearDown();
   void InitParameters();
-  void SetUpSeparationMatrix();
-  void TestInitState(separationmatrix::SeparationMatrix* fac);
-  void TestRequest(separationmatrix::SeparationMatrix* fac, double cap);
-  void TestAddMat(separationmatrix::SeparationMatrix* fac, 
+  void SetUpStreamBlender();
+  void TestInitState(streamblender::StreamBlender* fac);
+  void TestRequest(streamblender::StreamBlender* fac, double cap);
+  void TestAddMat(streamblender::StreamBlender* fac, 
       cyclus::Material::Ptr mat);
-  void TestBuffers(separationmatrix::SeparationMatrix* fac, double inv, double 
+  void TestBuffers(streamblender::StreamBlender* fac, double inv, double 
       proc, double rawbuff);
 
   std::string in_c1, out_c1, out_c2, out_c3;
@@ -33,6 +33,6 @@ class SeparationMatrixTest : public ::testing::Test {
   int process_time;
   double capacity, max_inv_size, cost;
 };
-} // namespace separationmatrix
-#endif // SEPARATIONMATRIX_TESTS_H_
+} // namespace streamblender
+#endif // STREAMBLENDER_TESTS_H_
 
